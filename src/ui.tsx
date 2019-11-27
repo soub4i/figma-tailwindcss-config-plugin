97fda98c46bff904064d40bceb0d9179bb2d9c51
Hamza Makraz <makraz.hamza@gmail.com>
2019-11-27 09:30:16 +0100

    update UI

--- src/ui.tsx
+++ src/ui.tsx
@@ -4,65 +4,71 @@ import "./ui.css";
 
 import * as template from "./template";
 interface IState {
-  data: Object;
+    data: Object;
 }
 
 interface IProps {}
 declare function require(path: string): any;
 
 class App extends React.Component<IProps, IState> {
-  textbox: HTMLInputElement;
+    textbox: HTMLInputElement;
 
-  constructor(props) {
-    super(props);
-    this.state = { data: {} };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { data: {} };
+    }
 
-  onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: "close-ui" } }, "*");
-  };
-
-  componentDidMount() {
-    parent.postMessage({ pluginMessage: { type: "scan-ui" } }, "*");
-
-    window.onmessage = event => {
-      let result = {
-        colors: event.data.pluginMessage.colorStyle,
-        ...event.data.pluginMessage.textStyle
-      };
-
-      this.setState({
-        data: { extends: result, ...template }
-      });
+    onCancel = () => {
+        parent.postMessage({ pluginMessage: { type: "close-ui" } }, "*");
     };
-  }
 
-  render() {
-    return (
-      <div>
-        <h2 className="title">Tailwindcss config Generator</h2>
+    componentDidMount() {
+        parent.postMessage({ pluginMessage: { type: "scan-ui" } }, "*");
 
-        <div className="grid-container">
-          <div className="grid-item">
-            <div className="code">
-              <h5>
-                <b>
-                  <a href="#you_stealer!_you-copied-my-code!">Copy</a>
-                </b>
-              </h5>
+        window.onmessage = event => {
+            let result = {
+                colors: event.data.pluginMessage.colorStyle,
+                ...event.data.pluginMessage.textStyle
+            };
 
-              <pre>{JSON.stringify(this.state.data, undefined, 2)}</pre>
+            this.setState({
+                data: { extends: result, ...template }
+            });
+        };
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="column">
+                        <h2 className="title">Tailwindcss config Generator</h2>
+                        <a
+                            className="button button-copy"
+                            href="#you_stealer!_you-copied-my-code!"
+                        >
+                            Copy
+                        </a>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="column">
+                        <pre className="code">
+                            {JSON.stringify(this.state.data, undefined, 2)}
+                        </pre>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="column">
+                        <button className="button button-generate">Generate</button>
+                        <button className="button button-cancel" onClick={this.onCancel}>Cancel</button>
+                    </div>
+                </div>
             </div>
-          </div>
-
-          <div className="grid-item"></div>
-        </div>
-
-        <button id="create">Generate</button>
-        <button onClick={this.onCancel}>Cancel</button>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById("react-page"));
