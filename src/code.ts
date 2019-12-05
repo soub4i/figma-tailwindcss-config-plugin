04b56229c4197c813f69642a3d78c38735719fea
Abderrhim Soubai <gx.coder@gmail.com>
2019-12-05 19:45:39 +0100

    fix: add new cases

--- src/code.ts
+++ src/code.ts
@@ -36,164 +36,110 @@ figma.ui.onmessage = async msg => {
       return hex + alpha;
     };
 
-    const walker = (node: BaseStyle) => {
-      if (!Array.from(["TEXT", "PAINT"]).includes(node.type)) {
-        return;
-      }
+    const handleText = (property, node) => {
+      textStyle[mapper[property]] = textStyle[mapper[property]] || {};
 
+      if (property === "fontName") {
+        let _value: FontName = (node as TextStyle)[property];
+
+        if (_value.family) {
+          Array.isArray(textStyle[mapper[property]])
+            ? textStyle[mapper[property]]["default"].push(_value.family)
+            : (textStyle[mapper[property]]["default"] = [_value.family]);
+        }
+      } else if (property === "fontSize") {
+        let _value: number = (node as TextStyle)[property];
+        textStyle[mapper[property]][`text-${_value}`] = `${_value}px'`;
+      } else if (property === "letterSpacing") {
+        let _value: LetterSpacing = (node as TextStyle)[property];
+        textStyle[mapper[property]][`tracking-${_value.value}`] = `${
+          _value.value
+        }${_value.unit === "PERCENT" ? "%" : "px"}`;
+      } else if (property === "textDecoration") {
+        if (typeof (node as TextStyle)[property] !== "symbol") {
+          let _value: TextDecoration = (node as TextStyle)[property];
+          textStyle[mapper[property]][
+            `${_value.toLocaleLowerCase()}`
+          ] = `${_value.toLocaleLowerCase()}`;
+        }
+      } else if (property === "lineHeight") {
+        let _value: any = (node as TextStyle)[property];
+        if (_value.unit && _value.unit === "AUTO") {
+          textStyle[mapper[property]][`leading-normal`] = `${_value.unit}`;
+        } else {
+          textStyle[mapper[property]][`leading-${_value.value}`] = `${
+            _value.value
+          }${_value.unit === "PERCENT" ? "%" : "px"}`;
+        }
+      } else {
+        textStyle[mapper[property]][node[property].name] = (node as TextStyle)[
+          property
+        ];
+      }
+    };
+
+    const handleEffect = node => {
+      let effects: ReadonlyArray<Effect> = (node as EffectStyle).effects;
+
+      for (let i = 0; i < effects.length; i++) {
+        let _value: any;
+        if (
+          effects[i].type === "DROP_SHADOW" ||
+          effects[i].type === "INNER_SHADOW"
+        ) {
+          _value = effects[0] as ShadowEffect;
+        } else {
+          _value = effects[0] as BlurEffect;
+        }
+        effectStyle[`shadow-${i}`] = `${
+          _value.type === "INNER_SHADOW" ? "inset" : ""
+        } ${_value.offset.x}px ${_value.offset.y}px ${_value.radius}px  rgba(${
+          _value.color.r
+        }, ${_value.color.g}, ${_value.color.b}, ${_value.color.a})`;
+      }
+    };
+
+    const handlePaint = node => {
+      let paints: ReadonlyArray<Paint> = (node as PaintStyle).paints;
+
+      for (let i = 0; i < paints.length; i++) {
+        if (paints[i].type === "SOLID") {
+          let _value: SolidPaint = paints[0] as SolidPaint;
+          colorStyle[`color-${i}`] = "#" + parseRGBA(_value.color);
+        }
+      }
+    };
+
+    const walker = (node: any) => {
       try {
         Object.keys(mapper).map(property => {
           if (property in node) {
-            if (node.type === "TEXT") {
-              textStyle[mapper[property]] = {};
+            switch (node.type) {
+              case "TEXT":
+                handleText(property, node);
 
-              if (property === "fontName") {
-                let _value: FontName = (node as TextStyle)[property];
+                break;
+              case "EFFECT":
+              case "FRAME":
+              case "RECTANGLE":
+              case "ELLIPSE":
+              case "GROUP":
+              case "COMPONENT":
+              case "INSTANCE":
+              case "LINE":
+              case "POLYGON":
+              case "STAR":
+              case "TEXT":
+              case "VECTOR":
+                handleEffect(node);
 
-                if (_value.family) {
-                  Array.isArray(textStyle[mapper[property]])
-                    ? textStyle[mapper[property]].push(_value.family)
-                    : (textStyle[mapper[property]] = [_value.family]);
-                }
-              } else if (property === "fontSize") {
-                let _value: number = (node as TextStyle)[property];
-                textStyle[mapper[property]][`text-${_value}`] = `${_value}px'`;
-              } else if (property === "letterSpacing") {
-                let _value: LetterSpacing = (node as TextStyle)[property];
-                textStyle[mapper[property]][`tracking-${_value.value}`] = `${
-                  _value.value
-                }${_value.unit === "PERCENT" ? "%" : "px"}`;
-              } else if (property === "textDecoration") {
-                if (typeof (node as TextStyle)[property] !== "symbol") {
-                  let _value: TextDecoration = (node as TextStyle)[property];
-                  textStyle[mapper[property]][
-                    `${_value.toLocaleLowerCase()}`
-                  ] = `${_value.toLocaleLowerCase()}`;
-                }
-              } else if (property === "lineHeight") {
-                let _value: any = (node as TextStyle)[property];
-                if (_value.unit && _value.unit === "AUTO") {
-                  textStyle[mapper[property]][
-                    `leading-normal`
-                  ] = `${_value.unit}`;
-                } else {
-                  textStyle[mapper[property]][`leading-${_value.value}`] = `${
-                    _value.value
-                  }${_value.unit === "PERCENT" ? "%" : "px"}`;
-                }
-              } else {
-                textStyle[mapper[property]][
-                  node[property].name
-                ] = (node as TextStyle)[property];
-              }
-            } else if (node.type === "PAINT") {
-              let paints: ReadonlyArray<Paint> = (node as PaintStyle).paints;
+                break;
+              case "PAINT":
+                handlePaint(node);
 
-              for (let i = 0; i < paints.length; i++) {
-                if (paints[i].type === "SOLID") {
-                  let _value: SolidPaint = paints[0] as SolidPaint;
-                  colorStyle[`color-${i}`] = "#" + parseRGBA(_value.color);
-                }
-              }
-            } else if (node.type === "EFFECT") {
-              let effects: ReadonlyArray<Effect> = (node as EffectStyle)
-                .effects;
-
-              for (let i = 0; i < effects.length; i++) {
-                let _value: any;
-                if (
-                  effects[i].type === "DROP_SHADOW" ||
-                  effects[i].type === "INNER_SHADOW"
-                ) {
-                  _value = effects[0] as ShadowEffect;
-                } else {
-                  _value = effects[0] as BlurEffect;
-                }
-                effectStyle[`shadow-${i}`] = `${
-                  _value.type === "INNER_SHADOW" ? "inset" : ""
-                } ${_value.offset.x}px ${_value.offset.y}px ${
-                  _value.radius
-                }px  rgba(${_value.color.r}, ${_value.color.g}, ${
-                  _value.color.b
-                }, ${_value.color.a})`;
-              }
-            } else {
-              if (mapper[property] === "effects") {
-                let effects: ReadonlyArray<Effect> = (node as EffectStyle)
-                  .effects;
-
-                for (let i = 0; i < effects.length; i++) {
-                  let _value: any;
-                  if (
-                    effects[i].type === "DROP_SHADOW" ||
-                    effects[i].type === "INNER_SHADOW"
-                  ) {
-                    _value = effects[0] as ShadowEffect;
-                  } else {
-                    _value = effects[0] as BlurEffect;
-                  }
-                  effectStyle[`shadow-${i}`] = `${
-                    _value.type === "INNER_SHADOW" ? "inset" : ""
-                  } ${_value.offset.x}px ${_value.offset.y}px ${
-                    _value.radius
-                  }px  rgba(${_value.color.r}, ${_value.color.g}, ${
-                    _value.color.b
-                  }, ${_value.color.a})`;
-                }
-              } else if (mapper[property] === "paints") {
-                let paints: ReadonlyArray<Paint> = (node as PaintStyle).paints;
-
-                for (let i = 0; i < paints.length; i++) {
-                  if (paints[i].type === "SOLID") {
-                    let _value: SolidPaint = paints[0] as SolidPaint;
-                    colorStyle[`color-${i}`] = "#" + parseRGBA(_value.color);
-                  }
-                }
-              } else {
-                textStyle[mapper[property]] = {};
-
-                if (property === "fontName") {
-                  let _value: FontName = (node as TextStyle)[property];
-
-                  if (_value.family) {
-                    Array.isArray(textStyle[mapper[property]])
-                      ? textStyle[mapper[property]].push(_value.family)
-                      : (textStyle[mapper[property]] = [_value.family]);
-                  }
-                } else if (property === "fontSize") {
-                  let _value: number = (node as TextStyle)[property];
-                  textStyle[mapper[property]][
-                    `text-${_value}`
-                  ] = `${_value}px'`;
-                } else if (property === "letterSpacing") {
-                  let _value: LetterSpacing = (node as TextStyle)[property];
-                  textStyle[mapper[property]][`tracking-${_value.value}`] = `${
-                    _value.value
-                  }${_value.unit === "PERCENT" ? "%" : "px"}`;
-                } else if (property === "textDecoration") {
-                  if (typeof (node as TextStyle)[property] !== "symbol") {
-                    let _value: TextDecoration = (node as TextStyle)[property];
-                    textStyle[mapper[property]][
-                      `${_value.toLocaleLowerCase()}`
-                    ] = `${_value.toLocaleLowerCase()}`;
-                  }
-                } else if (property === "lineHeight") {
-                  let _value: any = (node as TextStyle)[property];
-                  if (_value.unit && _value.unit === "AUTO") {
-                    textStyle[mapper[property]][
-                      `leading-normal`
-                    ] = `${_value.unit}`;
-                  } else {
-                    textStyle[mapper[property]][`leading-${_value.value}`] = `${
-                      _value.value
-                    }${_value.unit === "PERCENT" ? "%" : "px"}`;
-                  }
-                } else {
-                  textStyle[mapper[property]][
-                    node[property].name
-                  ] = (node as TextStyle)[property];
-                }
-              }
+                break;
+              default:
+                console.log(node);
             }
           }
         });
--- src/ui.tsx
+++ src/ui.tsx
@@ -4,114 +4,108 @@ import "./ui.css";
 
 import * as template from "./template";
 interface IState {
-    data: Object;
-    isNodesSelected: boolean;
-    copySuccess: string;
+  data: Object;
+  isNodesSelected: boolean;
 }
 
 interface IProps {}
 
 class App extends React.Component<IProps, IState> {
-    constructor(props) {
-        super(props);
-        this.state = { data: {}, isNodesSelected: true, copySuccess: "" };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { data: {}, isNodesSelected: true };
+  }
 
-    onCancel = () => {
-        parent.postMessage({ pluginMessage: { type: "close-ui" } }, "*");
-    };
+  onCancel = () => {
+    parent.postMessage({ pluginMessage: { type: "close-ui" } }, "*");
+  };
 
-    componentDidMount() {
-        parent.postMessage({ pluginMessage: { type: "scan-ui" } }, "*");
+  componentDidMount() {
+    parent.postMessage({ pluginMessage: { type: "scan-ui" } }, "*");
 
-        window.onmessage = event => {
-            if (event.data.pluginMessage.isNodesSelected === true) {
-                console.log(event.data.pluginMessage);
-                const result = {
-                    ...template
-                };
-                result.theme.extend = {
-                    colors: event.data.pluginMessage.colorStyle,
-                    boxShadow: event.data.pluginMessage.effectStyle,
-                    ...event.data.pluginMessage.textStyle
-                };
-                console.log(result);
-
-                this.setState({
-                    data: result,
-                    isNodesSelected: true
-                });
-            } else {
-                this.setState({
-                    isNodesSelected: false
-                });
-            }
+    window.onmessage = event => {
+      if (event.data.pluginMessage.isNodesSelected === true) {
+        console.log(event.data.pluginMessage);
+        const result = {
+          ...template
         };
-    }
+        result.theme.extend = {
+          colors: event.data.pluginMessage.colorStyle,
+          boxShadow: event.data.pluginMessage.effectStyle,
+          ...event.data.pluginMessage.textStyle
+        };
+        console.log(result);
 
-    copyToClipboard = e => {
-        var textField = document.getElementById("code");
-        var range = document.createRange();
-        var selection = window.getSelection();
-
-        range.selectNodeContents(textField);
-        selection.removeAllRanges();
-        selection.addRange(range);
-
-        document.execCommand("copy");
-
-        this.setState({ copySuccess: "Copied!" });
+        this.setState({
+          data: result,
+          isNodesSelected: true
+        });
+      } else {
+        this.setState({
+          isNodesSelected: false
+        });
+      }
     };
+  }
 
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="column">
-                        <h2 className="title">Tailwindcss config Generator</h2>
-                        {this.state.isNodesSelected === false ? (
-                            <br /> >
-                            (
-                                <h4 className="text-danger">
-                                    Select at least one node before you run the plugin
-                                </h4>
-                            )
-                        ) : (
-                            <span></span>
-                        )}
-                        {document.queryCommandSupported("copy") && (
-                            <button className="button button-copy" onClick={this.copyToClipboard}>
-                                Copy
-                            </button>
-                        )}
-                        {this.state.copySuccess}
-                    </div>
-                </div>
+  copyToClipboard = e => {
+    var textField = document.getElementById("code");
+    var range = document.createRange();
+    var selection = window.getSelection();
 
-                <div className="row">
-                    <div className="column">
-                        <pre className="code" id="code">
-                            {JSON.stringify(this.state.data, undefined, 2)}
-                        </pre>
-                    </div>
-                </div>
+    range.selectNodeContents(textField);
+    selection.removeAllRanges();
+    selection.addRange(range);
 
-                <div className="row">
-                    <div className="column">
-                        <button className="button button-generate">
-                            Generate
-                        </button>
-                        <button
-                            className="button button-cancel"
-                            onClick={this.onCancel}
-                        >
-                            Cancel
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    document.execCommand("copy");
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="column">
+            <h2 className="title">Tailwindcss config Generator</h2>
+            {this.state.isNodesSelected === false ? (
+              <br /> >
+              (
+                <h4 className="text-danger">
+                  Select at least one node before you run the plugin
+                </h4>
+              )
+            ) : (
+              <span></span>
+            )}
+            {document.queryCommandSupported("copy") && (
+              <button
+                className="button button-copy"
+                onClick={this.copyToClipboard}
+              >
+                Copy
+              </button>
+            )}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <pre className="code" id="code">
+              {JSON.stringify(this.state.data, undefined, 2)}
+            </pre>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <button className="button button-generate">Generate</button>
+            <button className="button button-cancel" onClick={this.onCancel}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
